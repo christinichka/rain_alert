7137cc4b6d67e5ec20d6d0806899ee3c1cfca432
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-01-30 15:33:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from twilio.rest import Client
 
-api_key = "a39893191f4756f82bc93259ed3d484a"
-account_sid = "AC47824f371eb909359194db481dddfaff"
-auth_token = "cdab63909e19f2bc3743cca0c110c2d5"
+api_key = xxxxxxxx
+account_sid = xxxxxxxx
+auth_token = xxxxxxxx
 
 # Parameters for the weather data
 parameters = {
@@ -32,8 +32,8 @@ if will_rain:
 	message = client.messages \
 		.create(
 			body="It's going to rain today. Remember to bring an ☂️",
-			from_="+19415640801", #You Twilio number
-			to="(314) 827-4781‬", #Your verified number
+			from_=xxxxxxxx, #You Twilio number
+			to=xxxxxxxx, #Your verified number
 	)
 	print(message.status)
 

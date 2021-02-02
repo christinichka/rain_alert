b5d3c8a39d88a8a2d3f58313bc2726bd79c819ba
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-02-01 19:28:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import requests
+import os
 from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
-api_key = xxxxxxxx
-account_sid = xxxxxxxx
-auth_token = xxxxxxxx
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = "AC47824f371eb909359194db481dddfaff"
+auth_token = os.environ.get("AUTH_TOKEN")
 
-# Parameters for the weather data
-parameters = {
-	"lat": 38.537201,
-	"lon": -90.313217,
-	"appid": api_key,
-	"exclude": "current,minutely,daily"
+weather_params = {
+    "lat": 38.537201,
+    "lon": -90.313217,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
-# Makes request to One Call API
-response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall?lat=38.537201&lon=-90.313217&appid=a39893191f4756f82bc93259ed3d484a", params=parameters)
-# If there is a problem and we don't get the correct code an exception will be made.
+response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 weather_slice = weather_data["hourly"][:12]
@@ -23,18 +23,23 @@ weather_slice = weather_data["hourly"][:12]
 will_rain = False
 
 for hour_data in weather_slice:
-	condition_code = hour_data["weather"][0]["id"]
-	if int(condition_code) < 700:
-		will_rain = True
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
 
-if will_rain:		
-	client = Client(account_sid, auth_token)
-	message = client.messages \
-		.create(
-			body="It's going to rain today. Remember to bring an ☂️",
-			from_=xxxxxxxx, #You Twilio number
-			to=xxxxxxxx, #Your verified number
-	)
-	print(message.status)
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_=XXXXXXXXXXX, #Your Twilio number
+        to=XXXXXXXXXXX, #Your personal phone number
+    )
+    print(message.status)
+
+
 
 
